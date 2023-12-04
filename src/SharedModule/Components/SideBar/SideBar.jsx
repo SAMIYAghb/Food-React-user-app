ff9978267e535d1e1940370ce0c10b58a444f8c9
samiya <samii-1989@live.com>
2023-12-04 16:13:26 +0100

    "user portal"

--- src/SharedModule/Components/SideBar/SideBar.jsx
+++ src/SharedModule/Components/SideBar/SideBar.jsx
@@ -9,7 +9,7 @@ const SideBar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const navigate = useNavigate();
 
-  // modal
+  // modale
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
